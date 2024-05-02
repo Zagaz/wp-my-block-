2387715a0c3fb838a01b0616bda71818320d2f84
Zagaz <andre_ranulfo@hotmail.com>
2024-05-01 23:29:29 -0300

    Descxription

--- wp-my-blocks.php
+++ wp-my-blocks.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       Wp My Blocks
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       A plugin to create custom blocks: A Title, Date, Content and Image.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
