a3056c970bf93d222f4a73182eabea689c932ac3
Zagaz <andre_ranulfo@hotmail.com>
2024-05-06 12:17:22 -0300

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '576017b7e7700ebfcee0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '51706361585d80371fcf');
